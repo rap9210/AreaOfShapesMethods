676cc61ed243657fb15c2c93476ddc78f40be97b
rap9210 <rap9210@gmail.com>
2020-05-05 09:11:46 -0400

    Forgot to add print line to area of square option, now fixed

--- src/AreaOfShapes.java
+++ src/AreaOfShapes.java
@@ -42,18 +42,19 @@ public class AreaOfShapes {
     }
     public static double circle_area(double radius){
         double area = 3.14*Math.pow(radius, 2);
-        System.out.println("The area of a circle with a radius \nof length "+radius+" is "+area);
+        System.out.println("The area of a circle with a radius \nof length "+radius+" is "+area+".");
     return area;}
     public static double triangle_area(double base, double height){
         double area = 0.5*base*height;
-        System.out.println("The area of a triangle with a base \nof "+base+"and a height of "+height+" is "+area);
+        System.out.println("The area of a triangle with a base \nof "+base+"and a height of "+height+" is "+area+".");
     return area;}
     public static double rectangle_area(double length, double width){
         double area = length*width;
-        System.out.println("The area of a rectangle with a length \nof "+length+" and a width of "+width+" is "+area);
+        System.out.println("The area of a rectangle with a length \nof "+length+" and a width of "+width+" is "+area+".");
     return area;}
     public static double square_area(double side_length){
         double area = Math.pow(side_length, 2);
+        System.out.println("The area of a square with a side length of "+side_length+" is "+area+".");
     return area;}
 
 }
